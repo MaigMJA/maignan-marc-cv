3e35092227c0a8b256b5b8cd662d7b3650355656
Marc Maignan <Marc Maignan>
2016-06-23 14:56:38 +0200

    all in one

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Pop portrait
+document.getElementsByClassName('pop')[0].addEventListener("click", function() {
+if(this.className === 'pop grow') {
+  this.className = 'pop';
+} else {
+  this.className = 'pop grow';
+}
+});
+//fin pop portrait
